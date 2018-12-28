77a0efb2e84f53261237965aeef39f3b3e29b196
Rumen Stoev <46021188+VidocqRR@users.noreply.github.com>
2018-12-28 11:50:33 +0000

    Create PythonFunction.py

---
+++ PythonFunction.py
@@ -0,0 +1,26 @@
+Artist = 'Bruno Mars';
+Genre = 'Reggae';
+Year = 2011;
+
+def ArtistName():
+    print('The name of Artist is:' + Artist)
+
+def GenreofMusic():
+    print('The Genre of the music is ' + Genre)
+
+def YearMusic():
+    print('The Years of the song is :' ,Year)
+
+ArtistName();
+
+GenreofMusic();
+
+YearMusic();
+
+
+def Boolean(Year):
+    if Year > 2010:
+        print(False)
+    else: print(True)
+
+Boolean(Year)
